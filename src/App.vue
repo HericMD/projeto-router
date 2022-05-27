fdd58960ff845cc91b3caea5e9d97b91406bfc5d
WedleySilva <wedleysilva2105@gmail.com>
2022-05-27 11:26:01 -0300

    master

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BlocoHeader from "@/components/BlocoHeader.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <BlocoHeader />
+  <RouterView />
+</template>
+
+<style></style>
