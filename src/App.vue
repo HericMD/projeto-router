01dd4b10d10e5f037896544d01c807408012ca50
WedleySilva <wedleysilva2105@gmail.com>
2022-05-30 11:18:01 -0300

    routerprojeto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script setup>
-import BlocoHeader from "@/components/BlocoHeader.vue";
-import { RouterView } from "vue-router";
-</script>
-
 <template>
-  <BlocoHeader />
-  <RouterView />
+  <header>
+    <span>Home</span>
+    <span class="separador">|</span>
+    <span class="texto-caixa">Conteúdo1</span>
+    <span class="separador">|</span>
+    <span class="texto-caixa">Conteúdo2</span>
+  </header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
 </template>
 
-<style></style>
+<style>
+/* @import "@/assets/base.css"; */
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+  height: 100vh;
+}
+header,
+footer {
+  height: 10%;
+  background-color: black;
+  color: white;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+}
+header span {
+  padding: 0 20px;
+}
+
+main {
+  height: 80%;
+}
+</style>
